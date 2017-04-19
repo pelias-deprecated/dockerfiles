dfca1338fe83db3c7e7cc4d203de132b04730495
Peter Johnson <insomnia@rcpt.at>
2017-04-19 15:41:00 +0200

    allthethings

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # bring containers down
-# note: the -v flag deletes persistent data volumes
-docker-compose down -v || true;
+# note: the -v flag deletes ALL persistent data volumes
+docker-compose down || true;
+
+# delete persistent volumes
+docker volume rm 'dockerfiles_esdata'
 
 # rebuild the images
 docker-compose build;
@@ -12,7 +15,7 @@ docker-compose build;
 docker-compose up -d;
 
 # wait for elasticsearch to start up
-echo 'waiting for elasticsearch service to come up...';
+echo 'waiting for elasticsearch service to come up';
 until $(curl --output /dev/null --silent --head --fail http://localhost:9200); do
   printf '.'
   sleep 2
