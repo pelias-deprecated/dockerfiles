6373410e92d3cee215b036d1f617796fc925ee47
Peter Johnson <insomnia@rcpt.at>
2018-04-24 12:21:09 +0200

    more intuative detection and resolution of DATA_DIR not set

--- cmd/system.sh
+++ cmd/system.sh
@@ -3,3 +3,6 @@ set -e;
 
 function system_check(){ env_check; }
 register 'system' 'check' 'ensure the system is correctly configured' system_check
+
+function system_env(){ printf 'DATA_DIR=%s\n' ${DATA_DIR}; }
+register 'system' 'env' 'display environment variables' system_env
