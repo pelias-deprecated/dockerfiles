8c1aacd8a28dce470a67be153265b745b5a6c4fc
Peter Johnson <insomnia@rcpt.at>
2018-06-11 14:53:11 +0200

    deprecation: deprecation notices

--- run_services.sh
+++ run_services.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# deprecation notice
+2>&1 echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
+2>&1 echo 'This repository has been deprecated in favour of https://github.com/pelias/docker';
+2>&1 echo;
+2>&1 echo 'We strongly recommended you to migrate any code referencing this repository';
+2>&1 echo 'to use https://github.com/pelias/docker as soon as possible.'
+2>&1 echo;
+2>&1 echo 'You can find more information about why we deprecated this code along with a migration guide in the wiki:';
+2>&1 echo 'https://github.com/pelias/dockerfiles/wiki/Deprecation-Notice';
+2>&1 echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
+
 # load DATA_DIR and other vars from docker-compose .env file
 export $(cat .env | xargs)
 
